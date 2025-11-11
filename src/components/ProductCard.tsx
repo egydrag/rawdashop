816ff3dc6d3e729b6b3c7b3de87c6651688b6637
Anas Muhammad <ans.2012.1.25@gmail.com>
2025-11-11 13:50:17 +0200

    ver 1.1

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -10,10 +10,11 @@ export default function ProductCard({ product }: { product: Product }) {
       <Image
         src={imageSrc}
         alt={product.name}
-        className="w-full h-64 object-cover"
+        className="w-full h-64 object-contain"
         width={400}
         height={400}
       />
+
       <div className="p-3">
         <h3 className="text-lg font-semibold">{product.name}</h3>
         <p className="text-pink-600 font-bold mt-2">{product.price} ج.م</p>
